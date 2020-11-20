0bf9a38cad5b523faef5dd2648747ed9096ff804
Choharim <jhl8586@naver.com>
2020-11-20 19:13:58 +0900

    Add paintToDoFn

--- todo.js
+++ todo.js
@@ -1,16 +1,36 @@
 const toDoForm = document.querySelector(".js-toDoForm"),
-toDoInput = form.querySelector("input"),
+toDoInput = toDoForm.querySelector("input"),
 toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
+let toDos = [];
+
 function paintToDo(todo){
-  console.log(todo);
+  const li = document.createElement("li"),
+  span = document.createElement("span"),
+  delBtn = document.createElement("button");
+  delBtn.innerText = "x";
+
+  toDoList.appendChild(li);
+  li.appendChild(span);
+  li.appendChild(delBtn);
+
+  span.innerText = todo;
+
+  const toDoObj = {
+    text:todo,
+    id: toDos.length + 1;
+  };
+
+  toDos.push(toDoObj);
 }
+
 function handleSubmit(event){
   event.preventDefault();
   const currentValue = toDoInput.value;
   paintToDo(currentValue);
+  toDoInput.value = "";
 }
 
 function loadToDos(){
