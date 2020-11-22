5d8461861b1a2e60a1f740a1c7a419544a424390
Choharim <jhl8586@naver.com>
2020-11-22 14:57:43 +0900

    Add getWeatherFn by using API

--- weather.js
+++ weather.js
@@ -1,4 +1,20 @@
+const weather = document.querySelector(".js-weather");
+
 const COORDS = "coords";
+const API_KEY = "0447da692cbea035a1448e596d3213f5";
+
+function getWeather(latitude,longitude){
+  fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`) //url호출하는 법, 데이터 요구, 새로고침 안해도 실시간 업데이트 됌, ``안에 https://하고 씀
+  .then(function(response){
+    return response.json();
+  }) //then은 데이터가 완전히 들어온 다음 함수를 실행함
+  .then(function(json){
+    const temperature = json.main.temp;
+    const place = json.name;
+
+    weather.innerText = `${temperature}°C at ${place}`;
+  });
+}
 
 function handleGeoError(){
   alert("can not access your location");
@@ -17,6 +33,7 @@ function handleGeoSucces(position){
     latitude:latitude
   };
   saveLocation(locationObj);
+  getWeather(latitude,longitude);
 }
 
 function askForCoods(){
@@ -28,6 +45,10 @@ function loadedCoords(){
 
   if(loadedCoords === null){
     askForCoods();
+  }
+  else{
+    const parsedCoords = JSON.parse(loadedCoords);
+    getWeather(parsedCoords.latitude,parsedCoords.longitude);
   }
 }
 
